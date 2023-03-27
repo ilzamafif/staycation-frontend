898b71847c06b68871490e0959b4fb12e846f3e6
Ilzam afif <ilzamafif27@gmail.com>
2023-03-27 15:38:47 +0700

    menyusun component Hero

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -5,8 +5,42 @@ import IconCities from 'assets/images/icons/ic_cities.svg'
 import IconTraveler from 'assets/images/icons/ic_traveler.svg'
 import IconTreasure from 'assets/images/icons/ic_treasure.svg'
 
+import Button from 'components/Button'
+
 export default function Hero(props) {
+  function showMostPicked() {
+    window.scrollTo({
+      top: props.refMostPicked.current.offsetTop - 30,
+      behavior: "smooth",
+    });
+  }
+
   return (
-    <div>Hero</div>
+    <section className="container pt-4">
+        <div className="row align-items-center">
+          <div className="col-auto pr-5" style={{ width: 530 }}>
+            <h1 className="font-weight-bold line-height-1 mb-3">
+              Forget Busy Work, <br />
+              Start Next Vacation
+            </h1>
+            <p
+              className="mb-4 font-weight-light text-gray-500 w-75"
+              style={{ lineHeight: "170%" }}
+            >
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </p>
+            <Button
+              className="btn px-5"
+              hasShadow
+              isPrimary
+              onClick={showMostPicked}
+            >
+              Show Me Now
+            </Button>
+          </div>
+
+        </div>
+      </section>
   )
 }
