c4eafd43fafd09ab9f0866b499d7a167a20df60b
Ilzam afif <ilzamafif27@gmail.com>
2023-03-27 15:32:36 +0700

    impoert image to hero js

---
+++ src/parts/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageHero from 'assets/images/img-hero.jpg'
+import ImageHero_ from 'assets/images/frame-img-hero.jpg'
+import IconCities from 'assets/images/icons/ic_cities.svg'
+import IconTraveler from 'assets/images/icons/ic_traveler.svg'
+import IconTreasure from 'assets/images/icons/ic_treasure.svg'
+
+export default function Hero(props) {
+  return (
+    <div>Hero</div>
+  )
+}
