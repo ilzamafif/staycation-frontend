3d89797c6bdf84b5616dbf830f678e53807e594f
Ilzam afif <ilzamafif27@gmail.com>
2023-03-27 14:01:19 +0700

    update color variable & parts logo (component)

---
+++ src/parts/IconText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from 'components/Button'
+
+import React from 'react'
+
+export default function IconText() {
+  return (
+    <Button className='brand-text-icon' href='' type='Link'>
+        Stay <span className='text-gray-900'>cation.</span>
+    </Button>
+  )
+}
