a3c6567d9e04185def39e2f0d2b9a5974c0fbf8a
Ilzam afif <ilzamafif27@gmail.com>
2023-03-28 11:00:45 +0700

    finis component parts categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Header from 'parts/Header'
 import Hero from 'parts/Hero'
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 import landingPage from "json/landingPage.json";
 
 export default class LandingPage extends Component {
@@ -18,6 +19,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
           refMostPicked={this.refMostPicked}
         ></MostPicked>
+        <Categories data={landingPage.categories}></Categories>
       </>
     );
   }
