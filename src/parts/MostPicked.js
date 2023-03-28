2db359c064bede708c4ae78c2c32d2868682c962
Ilzam afif <ilzamafif27@gmail.com>
2023-03-28 09:53:25 +0700

    display grid and simple grid markup

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,9 +4,13 @@ export default function MostPicked() {
   return (
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
-      <div className="row">
-        <div className="col-4"></div>
-        <div className="col-8"></div>
+      <div className="container-grid">
+        <div className="item column-4 row-2">
+          <div className="card">Card 1</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 2</div>
+        </div>
       </div>
     </section>
   );
