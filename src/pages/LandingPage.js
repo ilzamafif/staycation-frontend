71cacc71ba2b57bdf0b27c8857719b7810dd9e6a
Ilzam afif <ilzamafif27@gmail.com>
2023-03-28 09:45:23 +0700

    import img from figma and example gri bootsrap

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import Header from 'parts/Header'
 import Hero from 'parts/Hero'
-import landingPage from 'json/landingPage.json'
+import MostPicked from "parts/MostPicked";
+import landingPage from "json/landingPage.json";
 
 export default class LandingPage extends Component {
-  render () {
-    return(
+  render() {
+    return (
       <>
-       <Header {...this.props}></Header>
-       <Hero data={landingPage.hero}></Hero>
+        <Header {...this.props}></Header>
+        <Hero data={landingPage.hero}></Hero>
+        <MostPicked data={landingPage.mostPicked}></MostPicked>
       </>
-    )
-      
+    );
   }
 }
---
+++ src/parts/MostPicked.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MostPicked() {
+  return (
+    <section className="container">
+      <h4 className="mb-3">Most Picked</h4>
+      <div className="row">
+        <div className="col-4"></div>
+        <div className="col-8"></div>
+      </div>
+    </section>
+  );
+}
