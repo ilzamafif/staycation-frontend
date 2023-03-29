9a789f368efbd3530bb4cea95e6bd1135feaf780
Ilzam afif <ilzamafif27@gmail.com>
2023-03-29 16:49:23 +0700

    contoh input date

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import Example from "./pages/Example";
 import './assets/scss/style.scss';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/' element={<LandingPage/>}/>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/example" element={<Example />} />
         </Routes>
       </Router>
     </div>
