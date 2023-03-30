64669c3b7024812cf88ebf8651b76caaa5eba050
Ilzam afif <ilzamafif27@gmail.com>
2023-03-30 17:52:05 +0700

    adding detail page title to details page pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import DetailsPage from "./pages/DetailsPage";
 import Example from "./pages/Example";
-import './assets/scss/style.scss';
+import "./assets/scss/style.scss";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/example" element={<Example />} />
+          <Route exact path="/properties/:id" element={<DetailsPage />} />
+          <Route path="/example" element={<Example />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import Header from "parts/Header";
+import PageDetailTitle from "parts/PageDetailTitle";
+import itemDetails from "json/itemDetails.json";
+
+export default class DetailsPage extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    window.title = "Details Page";
+  }
+
+  render() {
+    const breadcrumb = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
+
+    return (
+      <>
+        <Header {...this.props} />
+        <PageDetailTitle breadcrumb={breadcrumb} data={itemDetails} />
+      </>
+    );
+  }
+}
