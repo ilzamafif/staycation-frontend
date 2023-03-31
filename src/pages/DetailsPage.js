93a0a9c19641cd4c741d4310749393809d40a201
Ilzam afif <ilzamafif27@gmail.com>
2023-03-31 09:53:41 +0700

    adding page detail description parts

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
+import PageDetailDescription from "parts/PageDetailDescription";
+
 import itemDetails from "json/itemDetails.json";
 
 export default class DetailsPage extends Component {
@@ -22,6 +24,16 @@ export default class DetailsPage extends Component {
         <Header {...this.props} />
         <PageDetailTitle breadcrumb={breadcrumb} data={itemDetails} />
         <FeaturedImage data={itemDetails.imageUrls}></FeaturedImage>
+        <div className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <PageDetailDescription data={itemDetails}></PageDetailDescription>
+            </div>
+            <div className="col-5">
+              <p>Booking</p>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
